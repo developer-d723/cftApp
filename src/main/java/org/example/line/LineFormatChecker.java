6925e3025540c17fdc3a33f9b798398ef2510cf7
developer-d723 <200369378+developer-d723@users.noreply.github.com>
2025-07-07 21:59:45 +0300

    feat: add LineFormatChecker and simple test for it

--- src/main/java/org/example/line/LineFormatChecker.java
+++ src/main/java/org/example/line/LineFormatChecker.java
@@ -2,7 +2,23 @@ package org.example.line;
 
 public class LineFormatChecker {
 
-    public CheckLineFormatResult check(String line) {
-        return null;
+    public static CheckLineFormatResult check(String line) {
+        if (line.isBlank()) {
+            return new CheckLineFormatResult(line, LineType.STRING);
+        }
+
+        try {
+            Integer.parseInt(line);
+            return new CheckLineFormatResult(line, LineType.INTEGER);
+        } catch (NumberFormatException ignored) {
+        }
+
+        try {
+            Float.parseFloat(line);
+            return new CheckLineFormatResult(line, LineType.FLOAT);
+        } catch (NumberFormatException ignored) {
+        }
+
+        return new CheckLineFormatResult(line, LineType.STRING);
     }
 }
---
+++ src/test/java/org/example/line/LineFormatCheckerTest.java
@@ -0,0 +1,41 @@
+package org.example.line;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class LineFormatCheckerTest {
+
+    @Test
+    void testIntegerValues() {
+        assertEquals(LineType.INTEGER, LineFormatChecker.check("0").lineType());
+        assertEquals(LineType.INTEGER, LineFormatChecker.check("0012").lineType());
+        assertEquals(LineType.INTEGER, LineFormatChecker.check("1234").lineType());
+        assertEquals(LineType.INTEGER, LineFormatChecker.check("-1234").lineType());
+        assertEquals(LineType.INTEGER, LineFormatChecker.check("+1234").lineType());
+    }
+
+    @Test
+    void testFloatValues() {
+        assertEquals(LineType.FLOAT, LineFormatChecker.check("0.00").lineType());
+        assertEquals(LineType.FLOAT, LineFormatChecker.check("1.2345").lineType());
+        assertEquals(LineType.FLOAT, LineFormatChecker.check("+0.001").lineType());
+        assertEquals(LineType.FLOAT, LineFormatChecker.check("-0.001").lineType());
+        assertEquals(LineType.FLOAT, LineFormatChecker.check("1.23456789E-25").lineType());
+        assertEquals(LineType.FLOAT, LineFormatChecker.check("+5.5").lineType());
+    }
+
+    @Test
+    void testStringValues() {
+        assertEquals(LineType.STRING, LineFormatChecker.check("abcde").lineType());
+        assertEquals(LineType.STRING, LineFormatChecker.check("12345abcd").lineType());
+        assertEquals(LineType.STRING, LineFormatChecker.check("1.2.3.4.5").lineType());
+        assertEquals(LineType.STRING, LineFormatChecker.check("1e3e5e5").lineType());
+        assertEquals(LineType.STRING, LineFormatChecker.check("1E-25.F").lineType());
+        assertEquals(LineType.STRING, LineFormatChecker.check("++12345").lineType());
+        assertEquals(LineType.STRING, LineFormatChecker.check("--12345").lineType());
+        assertEquals(LineType.STRING, LineFormatChecker.check("1,2345").lineType());
+    }
+
+
+}
